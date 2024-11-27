1c93408e545387e268c00778bc6f96e5cfcd857c
welru <reuelchristian.sundiam04@gmail.com>
2024-11-28 00:10:42 +0800

    DATA INTERPOLATION ON PERSONAL INFO COMP

--- src/app/personal-info/personal-info.component.ts
+++ src/app/personal-info/personal-info.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './personal-info.component.css'
 })
 export class PersonalInfoComponent {
+  name: string = 'Reuel Christian Sundiam';
+  profession: string = 'Web Developer';
   email: string = 'reuelchristian.sundiam04@gmail.com';
+  number: string = '09363112537'
 }
