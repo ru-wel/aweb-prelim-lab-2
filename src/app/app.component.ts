e006a4312a239cafd97f7c212e47fb67dd476f8e
welru <reuelchristian.sundiam04@gmail.com>
2024-11-28 01:03:27 +0800

    WORK EXPERIENCE COMP TO DO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PersonalInfoComponent } from "./personal-info/personal-info.component";
 import { EducationComponent } from './education/education.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PersonalInfoComponent, EducationComponent],
+  imports: [RouterOutlet, PersonalInfoComponent, EducationComponent, WorkExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
