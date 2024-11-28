2e9e2e11d750b62fa2dd9d6d64172f7bebc305d2
welru <reuelchristian.sundiam04@gmail.com>
2024-11-28 20:51:44 +0800

    SKILLS-CERT COMP

--- src/app/training/skills-certification/skills-certification.component.ts
+++ src/app/training/skills-certification/skills-certification.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+export interface certification{
+  certificate: string;
+  year: string;
+}
+
 @Component({
   selector: 'app-skills-certification',
   standalone: true,
@@ -7,6 +12,20 @@ import { Component } from '@angular/core';
   templateUrl: './skills-certification.component.html',
   styleUrl: './skills-certification.component.css'
 })
-export class SkillsCertificationComponent {
 
+export class SkillsCertificationComponent {
+  certificate: certification[] = [
+    {
+      certificate: 'Comptia ITF+',
+      year: '2023'
+    },
+    {
+      certificate: 'Cisco Certification in Introduction to Networks',
+      year: '2024'
+    },
+    {
+      certificate: 'Cisco Certification in Cybersecurity Essentials',
+      year: '2024'
+    }
+  ];
 }
